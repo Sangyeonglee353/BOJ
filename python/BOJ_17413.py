cfb1337bf6c6cdd9e810e45d937574fe6f1c6bbf
PatienceLee <Sangyeong.lee353@gmail.com>
2022-04-15 20:55:33 +0900

    BOJ Algorithm 17413 String reverse 2

---
+++ python/BOJ_17413.py
@@ -0,0 +1,44 @@
+# BOJ_17413, 단어 뒤집기 2
+import sys
+from collections import deque
+q = deque()
+stack = []
+result = ''
+state = True # 상태 변수
+
+input = sys.stdin.readline
+
+S = list(input().strip())
+
+for i in S:
+    if i == '<':                        # '<' 입력 받으면
+        while stack:                    # 그 전에 stack에 젖아되어 있던 것들 결과값에 저장
+            result += stack.pop()
+        q.append(i)                     # '<' 큐에 저장
+        state = False                   # <. > 안의 문자들은 큐에 저장해야 하므로 상태변수 False로 변경
+    
+    elif i == '>':                      # '>' 입력 받으면
+        q.append(i)                     # 큐에 저장
+        state = True                    # <. > 괄호가 끝났으므로 상태변수 True
+        while q:                        # 괄호 안에 있던 문자 모두 결과 값에 저장
+            result += q.popleft()       # 순서 그대로 나와야 하므로 deque에서 popleft()
+    
+    elif i == ' ':                      # 공백 입력 받았는데
+        if state:                       # 괄호 밖의 상태이면
+            while stack:                # stack에 저장되어 있는 것들 다 결과값에 저장
+                result += stack.pop()
+            result += ' '               # 공백 전에 문자들을 결과값에 저장해준 뒤 공백 저장
+        else:                           # <, > 괄호 안의 상태이면
+            q.append(i)                 # 큐에 공백 저장해준 뒤
+            while q:
+                result += q.popleft()
+    else:                               # 입력받은 것이 문자이면
+        if state:                       # 괄호 밖의 상황
+            stack.append(i)             # 스택에 저장
+        else:                           # 괄호 안의 상황
+            q.append(i)                 # 큐에 저장
+
+while stack:                            # 혹시 스택에 남아있는 문자 있다면
+    result += stack.pop()               # 모두 결과값에 저장
+
+print(result)
