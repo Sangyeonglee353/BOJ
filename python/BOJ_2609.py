c5584646cc2081675b8b4ebfd361d078dcaa115d
PatienceLee <Sangyeong.lee353@gmail.com>
2022-04-28 20:33:55 +0900

    BOJ Algorithm 2609 GCD LCM

---
+++ python/BOJ_2609.py
@@ -0,0 +1,15 @@
+# BOJ_2609 최대공약수와 최소공배수
+import sys
+input = sys.stdin.readline
+a, b = map(int, input().split())
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+print(gcd(a, b))
+print(lcm(a, b))
