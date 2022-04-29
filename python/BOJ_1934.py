bf287b6ff147101c28ae2ce5a3b1db98ee21ed4c
PatienceLee <Sangyeong.lee353@gmail.com>
2022-04-29 23:30:25 +0900

    BOJ Algorithm 1934 Least Common Muliple

---
+++ python/BOJ_1934.py
@@ -0,0 +1,17 @@
+# BOJ_1934 least common mulitple
+import sys
+input = sys.stdin.readline
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+N = int(input())
+
+for _ in range(N):
+    a, b = map(int, input().split())
+    print(lcm(a, b))
