3ef44cd7b6a1a533de54dd27ae144c77e442b9bd
PatienceLee <Sangyeong.lee353@gmail.com>
2022-05-13 20:19:50 +0900

    BOJ Algorithm 2745 notation

---
+++ python/BOJ_2745.py
@@ -0,0 +1,10 @@
+# BOJ_2745, 진법 변환
+N, b = input().split()
+ary = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+N = N[::-1]
+result = 0
+
+for i,n in enumerate(N):
+    result += (int(b)**i)*(ary.index(n))
+print(result)
