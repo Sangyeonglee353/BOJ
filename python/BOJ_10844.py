1cc6163c6f2a7facde8bd3ec3d3bb8255aefad5b
PatienceLee <Sangyeong.lee353@gmail.com>
2022-05-22 23:55:21 +0900

    BOJ Algorithm 10844 Stairs

---
+++ python/BOJ_10844.py
@@ -0,0 +1,19 @@
+# BOJ_10844, 쉬운 계단 수
+import sys
+import copy
+input = sys.stdin.readline
+n = int(input())
+dp = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+if n == 1:
+    print(9)
+else:
+    for j in range(1, n):
+        dp_copied = copy.deepcopy(dp)
+        for i in range(10):
+            if i == 0:
+                dp[i] = dp_copied[1]
+            elif i == 9:
+                dp[i] = dp_copied[8]
+            else:
+                dp[i] = dp_copied[i-1] + dp_copied[i+1]
+    print(sum(dp)%1000000000)
