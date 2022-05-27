4faf1c589591105e34d14b3f6beadaf9c812c9fb
PatienceLee <Sangyeong.lee353@gmail.com>
2022-05-27 23:25:35 +0900

    BOJ Algorithm 1699 sum of square number

---
+++ python/BOJ_1699.py
@@ -0,0 +1,15 @@
+# BOJ_1699, 제곱수의 합
+import sys
+input =sys.stdin.readline
+
+n=int(input())
+
+dp=[0]*(n+1)
+for i in range(1,n+1):
+    dp[i]=i
+    for j in range(1,i+1):
+        if j*j>i:
+            break
+        if dp[i] >dp[i-j*j]+1:
+            dp[i]=dp[i-(j*j)]+1 # dp[j**2]=1 이다. 
+print(dp[n])
