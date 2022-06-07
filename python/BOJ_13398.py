40ff962353ac615f8ad76ff6dfd75daa66d9103a
PatienceLee <Sangyeong.lee353@gmail.com>
2022-06-07 23:10:05 +0900

    BOJ Algorithm 13398 decompose

---
+++ python/BOJ_13398.py
@@ -0,0 +1,16 @@
+# BOJ_13398 연속합 2
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+res = [[0] * n for _ in range(2)]
+maxres = arr[0]
+if n == 1:
+    print(arr[0])
+else:
+    res[0][0] = arr[0]
+    for i in range(1, n):
+        res[0][i] = max(res[0][i - 1] + arr[i], arr[i])
+        res[1][i] = max(res[1][i - 1] + arr[i], res[0][i - 1])
+        maxres = max(maxres, res[0][i], res[1][i])
+    print(maxres)
