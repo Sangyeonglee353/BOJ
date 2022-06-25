439c60201104345ff2d68c2e28474576c19fe209
PatienceLee <Sangyeong.lee353@gmail.com>
2022-06-26 00:00:00 +0900

    BOJ Algorithm 15663 N & M (9)

---
+++ python/BOJ_15663.py
@@ -0,0 +1,17 @@
+# BOJ_15663, N과 M (9)
+def dfs(l):
+    if len(q) == M:
+        print(' '.join(map(str, q)))
+        return
+
+    for i in range(l, N):
+        q.append(a[i])
+        dfs(i)
+        q.pop()
+
+
+q = []
+N, M = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+dfs(0)
